8a21a2af30578549d28e93826418dc0f29abfa5e
Aaron McLeod <sircoolguy99@gmail.com>
2015-06-14 15:14:25 -0400

    search behaviour class

---
+++ core/src/com/agmcleod/sp/aibehaviours/SearchBehaviour.java
@@ -0,0 +1,17 @@
+package com.agmcleod.sp.aibehaviours;
+
+import com.agmcleod.sp.Enemy;
+
+/**
+ * Created by aaronmcleod on 15-06-14.
+ */
+public class SearchBehaviour extends Behaviour {
+    public SearchBehaviour(Enemy enemy) {
+        super(enemy);
+    }
+
+    @Override
+    public void update() {
+        
+    }
+}
