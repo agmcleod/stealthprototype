961ffc46e73da409b487485ce3a565153ad1f7a5
Aaron McLeod <sircoolguy99@gmail.com>
2015-07-11 15:05:11 -0400

    tweaking objects a bit

--- core/src/com/agmcleod/sp/HackableComponent.java
+++ core/src/com/agmcleod/sp/HackableComponent.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class HackableComponent extends GameObject {
