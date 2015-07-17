acfd7770d0c9016ed40b89a33cf53c943c1639b8
Aaron McLeod <aaron.mcleod@mosaic.com>
2015-07-17 16:06:17 -0400

    drop out the vector import

--- core/src/com/agmcleod/sp/HackAction.java
+++ core/src/com/agmcleod/sp/HackAction.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 
 /**
